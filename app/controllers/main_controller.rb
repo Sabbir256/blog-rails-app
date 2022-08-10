93d77ede7a090c58916d610bf7091e8457d7f5c7
sabbir <29686831+aboutsabbir@users.noreply.github.com>
2022-08-10 14:53:45 +0600

    login

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,6 @@ class MainController < ApplicationController
     def index
         flash[:alert] = "This is an alert to test flash [built in hashmap, like cache] in ruby!"
         @blogs = Blog.all
+        @user = User.find_by(id: session[:user_id])
     end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,7 @@ class RegistrationsController < ApplicationController
     def create
         @user = User.new(user_param)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to "/", notice: "User saved sucessfully!"
         else
             render :new
